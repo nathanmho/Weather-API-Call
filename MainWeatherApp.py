8f31c08af2c019f00f77e92bd997a501f1ff0a9a
kaimou1357 <kaimou1357@gmail.com>
2015-10-05 00:00:07 -0400

    Basic requests. Please check the temperature to make sure it's not wrong.

--- MainWeatherApp.py
+++ MainWeatherApp.py
@@ -1,12 +1,35 @@
 __author__ = 'Kai'
 
+from pprint import pprint
+import requests
+import json
+APIKey = "f5dfc4cfeadb862325cb3fe9fd4ad6e4"
 
 def main():
-    print("hello world")
+    print(callWeatherAPI())
 
 
-#Function to call the weather API and parse data. 
+
+#Function to call the weather API and parse data.
 def callWeatherAPI():
+    #Basic call to openweather map. Need to extract temperature/blehbleh bleh take user input.
+    #Change the params to take in user input later.
+    weatherParams = {'APPID' : APIKey, 'q' : "London"}
+    r = requests.get('http://api.openweathermap.org/data/2.5/weather?', params = weatherParams)
+    json_data = r.json()
+
+    return json_data['main']['temp']
+
+
+
+
+
+#Function to parse the user data.
+def parseCallBackString():
+    pass
+
+main()
+
 
 
 
